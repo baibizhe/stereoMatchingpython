d788aba573e7107e263c9018d1156038c9102b68
bbzatyddounan@gmail.com <Byl95899589!>
2021-07-05 16:52:18 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import sys
+from PyQt5 import QtWidgets
+from mainUI import Ui_Form
+
+class MyPyQT_Form(QtWidgets.QWidget,Ui_Form):
+    def __init__(self):
+        super(MyPyQT_Form,self).__init__()
+        self.setupUi(self)
+
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    my_pyqt_form = MyPyQT_Form()
+    my_pyqt_form.show()
+    sys.exit(app.exec_())
